8b4b8a54ffd094132f3f6d36dcdc9e0c1c68751c
SashaBenzar <sahabenzar@gmail.com>
2024-01-12 15:19:56 +0200

    fix#1

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,34 +1,6 @@
 import * as Yup from "yup";
 import { ErrorMessages } from "./message";
 
-export const signUpValidation = Yup.object().shape({
-  username: Yup.string().required(ErrorMessages.UserNameEmpty),
-  email: Yup.string().email(ErrorMessages.EmailError).required(ErrorMessages.EmailEmpty),
-  password: Yup.string()
-    .min(4, ErrorMessages.PasswordError)
-    .max(15, ErrorMessages.PasswordError)
-    .required(ErrorMessages.PasswordEmpty),
-  isAdmin: Yup.boolean(),
-  picture: Yup.mixed()
-    .nullable()
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    .test("type", ErrorMessages.PictureError, (value: any) => {
-      return value ? value.type === "image/jpeg" || value.type === "image/png" : true;
-    })
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    .test(ErrorMessages.PictureTooBig, (value: any) => {
-      return value ? value?.size <= 1024 * 1024 * 5 : true;
-    }),
-});
-
-export const signInValidation = Yup.object().shape({
-  email: Yup.string().email(ErrorMessages.EmailError).required(ErrorMessages.EmailEmpty),
-  password: Yup.string()
-    .min(4, ErrorMessages.PasswordError)
-    .max(15, ErrorMessages.PasswordError)
-    .required(ErrorMessages.PasswordEmpty),
-});
-
 export const EditUserValidation = Yup.object().shape({
   username: Yup.string().required(ErrorMessages.UserNameEmpty),
   email: Yup.string().email(ErrorMessages.EmailError).required(ErrorMessages.EmailEmpty),
@@ -43,4 +15,19 @@ export const EditUserValidation = Yup.object().shape({
     .test(ErrorMessages.PictureTooBig, (value: any) => {
       return value ? value?.size <= 1024 * 1024 * 5 : true;
     }),
+});
+
+export const signUpValidation = EditUserValidation.shape({
+  password: Yup.string()
+    .min(4, ErrorMessages.PasswordError)
+    .max(15, ErrorMessages.PasswordError)
+    .required(ErrorMessages.PasswordEmpty),
+});
+
+export const signInValidation = Yup.object().shape({
+  email: Yup.string().email(ErrorMessages.EmailError).required(ErrorMessages.EmailEmpty),
+  password: Yup.string()
+    .min(4, ErrorMessages.PasswordError)
+    .max(15, ErrorMessages.PasswordError)
+    .required(ErrorMessages.PasswordEmpty),
 });
