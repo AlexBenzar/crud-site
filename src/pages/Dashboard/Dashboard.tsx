2b2ef6c8289a0282e4dd8e63c4a912eed2a56682
SashaBenzar <sahabenzar@gmail.com>
2024-01-18 10:28:24 +0200

    fix#1

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,39 +3,28 @@ import { DashboardUsers, DashboardProfiles, DashboardAge } from "img";
 import styles from "./Dashboard.module.scss";
 
 export const Dashboard: React.FC = () => {
+  const info = [
+    { img: DashboardUsers, key: "Users", value: 400 },
+    { img: DashboardProfiles, key: "Profiles", value: 500 },
+    { img: DashboardAge, key: "Users 18+", value: 50 },
+  ];
   return (
     <div className={styles.dashboard}>
       <Typography variant="title-2" className={styles.dashboard__title}>
         Dashboard
       </Typography>
       <div className={styles.dashboard__card}>
-        <div className={styles.dashboard__stat}>
-          <Typography variant="body-1" className={styles.dashboard__category}>
-            <img src={DashboardUsers} alt="Users" />
-            Users
-          </Typography>
-          <Typography variant="body-3" className={styles.dashboard__result}>
-            400
-          </Typography>
-        </div>
-        <div className={styles.dashboard__stat}>
-          <Typography variant="body-1" className={styles.dashboard__category}>
-            <img src={DashboardProfiles} alt="Profiles" />
-            Profiles
-          </Typography>
-          <Typography variant="body-3" className={styles.dashboard__result}>
-            500
-          </Typography>
-        </div>
-        <div className={styles.dashboard__stat}>
-          <Typography variant="body-1" className={styles.dashboard__category}>
-            <img src={DashboardAge} alt="Age" />
-            Users 18+
-          </Typography>
-          <Typography variant="body-3" className={styles.dashboard__result}>
-            50
-          </Typography>
-        </div>
+        {info.map((data, index) => (
+          <div className={styles.dashboard__stat} key={index}>
+            <Typography variant="body-1" className={styles.dashboard__category}>
+              <img src={data.img} alt="Users" />
+              {data.key}
+            </Typography>
+            <Typography variant="body-3" className={styles.dashboard__result}>
+              {data.value}
+            </Typography>
+          </div>
+        ))}
       </div>
     </div>
   );
