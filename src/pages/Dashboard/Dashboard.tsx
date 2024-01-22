d2e617a0a457b5c2243f5bc5a3a2dfa76e1a064a
SashaBenzar <sahabenzar@gmail.com>
2024-01-22 09:01:16 +0200

    fixed loaders

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -4,15 +4,15 @@ import { useGetDashboardDataQuery } from "store/slices/authSlice";
 import styles from "./Dashboard.module.scss";
 
 export const Dashboard: React.FC = () => {
-  const { data, isFetching, isLoading } = useGetDashboardDataQuery();
+  const { data, isFetching, isLoading, isError } = useGetDashboardDataQuery();
   const info = [
     { img: DashboardUsers, key: "Users", value: data?.sumOfUsers },
     { img: DashboardProfiles, key: "Profiles", value: data?.sumOfProfiles },
     { img: DashboardAge, key: "Users 18+", value: data?.sumOfProfilesOlderThen18 },
   ];
 
-  if (isLoading || isFetching) {
-    return <Loader />;
+  if (isError) {
+    return <Typography variant="error-1">Error occur</Typography>;
   }
 
   return data ? (
@@ -21,20 +21,24 @@ export const Dashboard: React.FC = () => {
         Dashboard
       </Typography>
       <div className={styles.dashboard__card}>
-        {info.map((data, index) => (
-          <div className={styles.dashboard__stat} key={index}>
-            <Typography variant="body-1" className={styles.dashboard__category}>
-              <img src={data.img} alt="Users" />
-              {data.key}
-            </Typography>
-            <Typography variant="body-3" className={styles.dashboard__result}>
-              {data.value}
-            </Typography>
-          </div>
-        ))}
+        {isLoading || isFetching ? (
+          <Loader />
+        ) : (
+          info.map((data, index) => (
+            <div className={styles.dashboard__stat} key={index}>
+              <Typography variant="body-1" className={styles.dashboard__category}>
+                <img src={data.img} alt="Users" />
+                {data.key}
+              </Typography>
+              <Typography variant="body-3" className={styles.dashboard__result}>
+                {data.value}
+              </Typography>
+            </div>
+          ))
+        )}
       </div>
     </div>
   ) : (
-    <Typography variant="error-1">Error occur</Typography>
+    <Loader />
   );
 };
